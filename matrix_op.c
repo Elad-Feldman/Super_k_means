5cf8f3d85344af06c94eb37a7a81bbf45dc44853
Elad-Feldman <eladfeldman@mail.tau.ac.il>
2021-07-24 09:32:26 +0300

    comments on code 2

--- matrix_op.c
+++ matrix_op.c
@@ -5,34 +5,75 @@
 #include<math.h>
 #include "matrix_op.h"
 
+#define Arr_size(x)  (sizeof(x) / sizeof((x)[0]))
 
-void free_matrix( double  ** A, int N)
-{
+
+/*************** implementation Vector ******************/
+
+double dot_mult_vector(double *a, double *b, int n) {
+    assert(a);
+    assert(b);
+    assert(n > 0);
+    double sum;
     int i;
-    for (i = 0; i < N; i++)
-        free(A[i]);
-    free(A);
-}
-
-
-void print_mat( double  ** mat, int N, int d)
-{
-    int i, j;
-    for (i = 0; i < N; i++) {
-        printf("[");
-        for (j = 0; j < d; j++)
-            printf("%.4f,", mat[i][j]);
-        printf("]\n");
+    sum = 0;
+    for (i = 0; i < n; i++)
+    {
+        sum += a[i] * b[i];
     }
+    return sum;
+}
+
+double find_vec_norm(double* a, int n) {
+    int i;
+    double sum;
+    assert(a);
+    assert(n > 0);
+    sum = 0;
+    for ( i = 0; i < n; i++)
+    {
+        sum += (a[i] * a[i]);
+    }
+    return sqrt(sum);
+}
+
+double find_vec_norm_diff(double* a, double* b, int n) {
+     /* returns the euclidian distance bitween two vectors a, b */
+    int i;
+    double norm;
+    double * c =  (double  *) calloc(n,sizeof(double));
+    for (i = 0; i < n; i++)
+        c[i]= a[i]-b[i];
+    norm = find_vec_norm(c,n);
+    free(c);
+    return norm;
+}
+
+double sum_vector(double a[], int n) {
+    int sum, i;
+    sum = 0;
+    for (i = 0; i < n; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+void print_vector(double* a,int n){
+    int i;
+    printf("[");
+    for (i = 0; i < n; i++)
+        printf("%.0f,",a[i]);
+    printf("]\n");
 }
 
 
- double **create_matrix(int N, int d)
- {
-    double** mat = (double  **)calloc(N , sizeof(double*));
-     assert(mat);
+/*************** implementation MATRIX ******************/
+double **create_matrix(int n, int d)
+{
+    double** mat = (double  **)calloc(n , sizeof(double*));
+    assert(mat);
     int i;
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < n; i++) {
         mat[i] = (double  *) calloc(d,sizeof(double));
         assert(mat[i]);
 
@@ -40,177 +81,97 @@ void print_mat( double  ** mat, int N, int d)
     return mat;
 
 }
- //creates a matrix such that every element on the diagon is 1 and all other elements are 0
- double** create_Id_matrix(int N) {
-     assert(N > 0);
-     double** mat = create_matrix(N,N);
+
+void free_matrix( double  ** A, int n)
+{
     int i;
-     for (i = 0; i < N; i++) {
-         mat[i][i] = 1;
-     }
-     return mat;
- }
- //transpose matrix
- double** transpose_mat(double** mat, int N, int D) {
-     assert(mat);
-     assert(N > 0);
-     assert(D > 0);
-     double** mat_T = create_matrix(D, N);
-     int i, j;
-     for (i = 0; i < N; i++)
-     {
+    for (i = 0; i < n; i++)
+        free(A[i]);
+    free(A);
+}
 
-         for (j = 0; j < D; j++)
-         {
-             mat_T[j][i] = mat[i][j];
-         }
-     }
-     return mat_T;
- }
- //scalar multiplication
- double dot_vector(double a[], double b[], int n) {
-     assert(a);
-     assert(b);
-     assert(n > 0);
-     double sum;
-     int i;
-     sum = 0;
-     for (i = 0; i < n; i++)
-     {
-         sum += a[i] * b[i];
-     }
-     return sum;
- }
- 
- //vectorial multiplication
- double** mult_matrix(double** A, double** B,int N) {
-     assert(A);
-     assert(B);
-     assert(N > 0);
-     double** mat = create_matrix(N, N);
-     double** B_T = transpose_mat(B, N, N);
-     int i, j;
-     for (i = 0; i < N; i++)
-     {
-         for (j = 0; j < N; j++)
-         {
-             mat[i][j] = dot_vector(A[i], B_T[j],N);
-         }
-     }
-     return mat;
- }
- //finding the biggest element that is not on the diagon
- double find_max_ele_off_diag(double** A, int N) {
-     assert(A);
-     assert(N > 0);
-     int i, j;
-     double max = 0;
+double** transpose_mat(double** mat, int n, int D)
+{
+    assert(mat);
+    assert(n > 0);
+    assert(D > 0);
+    double** mat_T = create_matrix(D, n);
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < D; j++)
+        {
+            mat_T[j][i] = mat[i][j];
+        }
+    }
+    return mat_T;
+}
 
-     for (i = 0; i < N; i++)
-     {
+void mult_matrix(double** A, double** B, double ** C ,int n) {
+    assert(A);
+    assert(B);
+    assert(C);
+    assert(n > 0);
 
-         for (j = 0; j < N; j++)
-         {
-             if (i != j && max < A[i][j]) {
-                 max = A[i][j];
-             }
-         }
-     }
-     return max;
- }
- //checks if matrix is diagonal
- int is_diagonal(double** A, int N) {
-     assert(A);
-     assert(N > 0);
-     int i, j;
-     for (i = 0; i < N; i++) 
-     {
-         for (j = 0; j < N; j++)
-         {
-             if (i != j && A[i][j] != 0) {
-                 return 0;
-             }
-         }
-     }
-     return 1;
- }
- double find_vec_norm(double* a, int n) {
-     int i;
-     double sum;
-     assert(a);
+    double** B_T = transpose_mat(B, n, n);
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            C[i][j] = dot_mult_vector(A[i], B_T[j], n);
+        }
+    }
+    free_matrix(B_T,n);
+}
+
+void sub_matrix(double** A, double** B, double** C,int n) {
+    int i, j;
+    for ( i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            C[i][j] = A[i][j] - B[i][j];
+    }
+
+}
+
+void print_mat( double  ** mat, int n, int d)
+{
+    int i, j;
+    printf("==============================\n");
+    for (i = 0; i < n; i++) {
+        printf("[");
+        for (j = 0; j < d; j++)
+            printf("%.4f,", mat[i][j]);
+        printf("]\n");
+    }
+    printf("==============================\n");
+}
+
+double** create_Id_matrix(int n) {
      assert(n > 0);
-     sum = 0;
-     for ( i = 0; i < n; i++)
-     {
-         sum += (a[i] * a[i]);
-     }
-     return sqrt(sum);
- }
-//returns a normlized vector
- double* renorm_vec(double* a, int n) {
-     assert(a);
-     assert(n > 0);
-     int i;
-     double norm = find_vec_norm(a, n);
-     double* norm_vec = calloc(n, sizeof(double));
-     if (norm == 0) {
-         return norm_vec;
-     }
-    
+     double** mat = create_matrix(n,n);
+    int i;
      for (i = 0; i < n; i++) {
-         norm_vec[i] = (a[i] / norm);
-     }
-     return norm_vec;
- }
- //normalizes a matrix's columns
- double** renorm_matrix_cols(double** U, int N) {
-     double** renorm_mat = create_matrix(N, N);
-     int i,j;
-     double* norm_vec;
-     for (i = 0; i < N; i++)
-     {
-         norm_vec = renorm_vec(U[i], N);
-         for (j = 0; j < N; j++) {
-             renorm_mat[i][j] = norm_vec[j];
-         }
-     }
-     free(norm_vec);
-     return renorm_mat;
- 
- }
-//returns the euclidian distance bitween two vectors a, b
- double find_vec_norm_diff(double* a, double* b, int n) {
- 
- }
- int main(int argc, char* argv[]) {
-     int i, j;
-     int N = 6;
-     int D = 6;
-     double** mat = create_matrix(N, D);
-     double** id_mat = create_Id_matrix(N);
-     int count;
-     count = 1;
-     for ( i = 0; i < N; i++)
-     {
+         mat[i][i] = 1; /*  for i=\=j ,calloc  allocated memory block to zero */
 
-         for (j = 0; j < D; j++)
-         {
-             mat[i][j] = count;
-             count++;
-         }
      }
-     double** mat_renormed = renorm_matrix_cols(mat,N);
-     print_mat(mat, N, N);
-     printf("\n");
-     print_mat(mat_renormed, N, N);
-
-     return 0;
+     return mat;
  }
+
+
+/***************  SPK functions ******************/
+
+
+
+
  
+
 /*
- //  TODO implement
+   TODO implement
+    1.to optimize adj_weighted_mat
+    implement jacobi algorithim
 
-// Vector
-double find_vec_norm_diff(a double [],b double [], int n);
-double sum_vector (a double[],int n);
+
 */
+
